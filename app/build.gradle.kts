6ce62be1be0eb8618dca6050ecf093fa3fcb6132
felixjhonata <50239403+felixjhonata@users.noreply.github.com>
2026-04-11 13:05:33 +0700

    Add MockK and Kotlinx Coroutines Test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -106,11 +106,18 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.compose.material.icons.core)
     implementation(libs.androidx.compose.material.icons.extended)
+
+    // MockK
+    testImplementation(libs.mockk)
+    testImplementation(libs.kotlinx.coroutines.test)
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
